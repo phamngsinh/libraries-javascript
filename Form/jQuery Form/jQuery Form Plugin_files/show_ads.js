613e8f1de72fe9c6b199cc9067b09a4c7654bd3f
Sinh Pham <smagic39@gmail.com>
2015-01-18 22:30:48 +0700

    splite

---
+++ Form/jQuery Form/jQuery Form Plugin_files/show_ads.js
@@ -0,0 +1,96 @@
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<HTML><HEAD><TITLE>Endian Firewall - Access denied</TITLE>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<style type="text/css" media="screen">
+/* Stylesheet for endian firewall */
+/* Author: Raphael Vallazza */
+/* body */
+body {
+	margin: 0;
+	padding: 0;
+	padding-top: 3px;
+	background-color: #FFFFFF;
+	font-family: tahoma,verdana,arial,sans-serif;
+	background-repeat: repeat-x;
+	font-size: 11px;
+	color: #444;
+	min-width: 760px;
+}
+td {
+	font-family: tahoma,verdana,arial,sans-serif;
+	background-repeat: repeat-x;
+	font-size: 11px;
+	color: #444;
+	min-width: 760px;
+}
+h1, h2 {
+	font-size: 16px;
+	font-weight: bold;
+	color: #ca232a;
+	padding: 0;
+	margin: 0;
+	margin-bottom: 10px;
+}
+h3 {
+	width: inherit;
+	background-repeat: no-repeat;
+	border-right: 1px solid #ccc;
+	font-size: 10px;
+	font-weight: bold;
+	color: #656565;
+	margin: 0;
+	padding-left: 30px;
+	line-height: 20px;
+	display: block;
+}
+h4, h5 {
+	font-size: 12px;
+	font-weight: bold;
+	color: #ca232a;
+}
+a {
+    text-decoration: underling;
+    color: #ca232b;
+}
+a:hover {
+	text-decoration: underline;
+	color: #000000;
+}
+	    					
+table {
+	border: 1px solid #ccc;
+}
+</style>
+</HEAD>
+<BODY>
+<!--
+Url: http://pagead2.googlesyndication.com/pagead/show_ads.jsReason: Banned site: pagead2.googlesyndication.com 
+Reasonlogged: Banned site: pagead2.googlesyndication.comUser: 127.0.0.2IP: 192.168.0.35Filtergroup: Chan theo content content2Bypass: -->
+<br>
+<br>
+<CENTER>
+<table cellpadding="20" bgcolor="#efefef">
+<tr>
+<td align="center">
+<H1>Access has been denied 127.0.0.2!</H1>
+ Access to the page:
+<P>
+<strong><a href="http://pagead2.googlesyndication.com/pagead/show_ads.js">http://pagead2.googlesyndication.com/pagead/show_ads.js</a></strong>
+<P> ... has been denied for the following reason: <P>
+<strong><font color="#000000">Banned site: pagead2.googlesyndication.com</font></strong>
+<br>
+<br>
+You are seeing this error because what you attempted to access appears to contain,<br>
+or is labeled as containing, material that has been deemed inappropriate.
+<p>
+If you have any queries contact your ICT Co-ordinator or Network Manager. 
+</p>
+</td>
+</tr>
+</table>
+<P><font size=-3><a href="http://www.endian.it/">Endian Firewall</a> - Powered by <a href="http://dansguardian.org" target="_blank">DansGuardian</a></font>
+</center>
+</BODY>
+</HTML>
